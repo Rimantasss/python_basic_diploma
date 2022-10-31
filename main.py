82131f0b8d2d8ad28cc8464c7fef09c6f62af3d8
Rimas <dudonisrimas1993@gmail.com>
2022-10-31 21:49:52 +0300

    Done all branch

--- main.py
+++ main.py
@@ -7,9 +7,12 @@ from loguru import logger
 
 if __name__ == '__main__':
     logger.add('logs.log', format='{time} {level} {message}', level='DEBUG')
-    bot.add_custom_filter(StateFilter(bot))
-    set_default_commands(bot)
-    bot.infinity_polling()
+    try:
+        bot.add_custom_filter(StateFilter(bot))
+        set_default_commands(bot)
+        bot.infinity_polling()
+    except Exception as exc:
+        logger.exception(exc)
 
 
 
